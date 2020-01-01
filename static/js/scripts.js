d1aad0b1fb50d59e90670184c8b27f8e58c1c083
Elias <egomez@informat.com>
2020-01-01 18:37:48 -0300

    loading

---
+++ static/js/scripts.js
@@ -0,0 +1,25 @@
+function loading(config) {
+
+  var temp = ''
+  temp += '<div class="text-center">'
+  // temp += '<div><i class="fas fa-spinner fa-pulse fa-3x"></i></div>'
+  temp += '<div><i class="fas fa-theater-masks fa-pulse fa-3x"></i></div>'
+  temp += '<h1>@title<h1>'
+  temp += '<p>@decription</p>'
+  temp += '</div>'
+  temp = temp.replace(/@title/g, config.title == null ? 'Por favor espere' : config.title);
+  temp = temp.replace(/@decription/g, config.description == null ? '' : config.description);
+
+  if (config.active) {
+    $.blockUI({
+      message: temp,
+      css: {
+        backgroundColor: 'none',
+        color: '#fff',
+        border: 'none',
+      }
+    });
+  } else {
+    $.unblockUI();
+  }
+}
